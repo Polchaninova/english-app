b0c10836197b7e6ea7e330b65ee002abbc83f0e8
Olha Polchaninova <polchaninovaolha089@gmail.com>
2024-10-12 09:44:42 +0300

    init: initialize React project and add Material-UI library

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+
+
+function App() {
+  return (
+    <>
+
+        <Button variant="contained" >
+          Get Started
+        </Button>
+
+    </>
+  );
+}
+
+export default App;
