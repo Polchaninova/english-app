745cc3fe53889b3cda201e0a2065cfb0d5eda4a0
Olha Polchaninova <polchaninovaolha089@gmail.com>
2024-10-14 19:03:49 +0300

    feat: add style to start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import "@fontsource/poppins/300.css";
-import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/500.css";
-import "@fontsource/poppins/700.css";
 import StartPage from "./pages/start/Start";
 function App() {
   return (
--- src/pages/start/Start.jsx
+++ src/pages/start/Start.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import StartImg from "../../assets/woman.png";
+import { Box, Typography, Button, ThemeProvider, createTheme } from "@mui/material";
 
 function Start() {
   return (
-    <>
-      <img src="" alt="hello"/>
-      <h1>Start your journey to job success today!</h1>
-      <Button variant="contained">Log In</Button>
-    </>
+    <ThemeProvider theme={theme}>
+      <Box align="center" sx={{ pt: 20, pb: 20 }}>
+        <Box
+          component="img"
+          src={StartImg}
+          alt="React Logo"
+          sx={{ mb: 2 }}
+        ></Box>
+
+        <Typography component="h3" variant="h3" color="primary" sx={{ mb: 4 }}>
+          Welcome to JobFluent
+        </Typography>
+
+        <Button variant="contained">Log In</Button>
+      </Box>
+    </ThemeProvider>
   );
 }
-
 export default Start;
